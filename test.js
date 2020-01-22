d587a34cd6b89323b8843136a716b5583a05a22f
Ankit Kumar <ankitkumar@mydomain-2.mydomain.com>
2020-01-22 19:08:48 +0530

    latest commit

---
+++ test.js
@@ -0,0 +1,168 @@
+let check = 0;
+//check 0 edit and delete buttons set to save and cancel
+//check 1  save and cancel buttons set to edit and delete
+let deleted_row = [];
+let datalen;
+$(document).ready(function() {
+    console.log("ready!");
+    $("#LOAD").click(function()
+{
+    $.getJSON("Employee.json",function(obj){
+        $.each(obj,function(key,value)
+        {
+            call(value);//here value is the array of objects 
+        });
+    });
+
+});
+
+});
+
+function call(Emp)
+{
+        document.getElementById("LOAD").innerHTML= "REFRESH DATA";
+        
+        let div = document.getElementById("id1");
+        div.innerHTML=" ";
+        CreateTable(Emp); 
+    //     while(div.firstChild)
+    //     {
+    //         div.removeChild(div.firstChild);
+    //     }
+}
+function CreateTable(Emp) {
+
+    let table = document.createElement('table');
+    table.setAttribute('id', 'empTable');
+
+    let arrHeaders = new Array();
+    arrHeaders = ['FirstName','MiddleName','LastName','Email','Phoneno','Role','Address'];
+    let arrValue = new Array();
+    for(var i in Emp) {
+    // i is the property name
+    arrValue.push(Emp[i]);
+    }
+for(let x =0;x<datalen;x++)
+{
+    deleted_row[x]=false;
+}
+    datalen=Emp.length;
+
+    let tr = table.insertRow(-1);
+    //create Headers
+    for (let h = 0; h < arrHeaders.length; h++) {
+        let th = document.createElement('th');              
+        th.innerHTML = arrHeaders[h];
+        tr.appendChild(th);
+    }
+    
+//    console.log(arr_len);
+    for (let c = 0; c <datalen; c++) {
+        tr = table.insertRow(-1);
+        tr.setAttribute("id","row"+c);
+        tr.innerHTML = 
+        '<td contenteditable="false" class="cell'+c+'">' + arrValue[c].FirstName + '</td>' +
+        '<td contenteditable="false" class="cell'+c+'">' + arrValue[c].MiddleName + '</td>' +
+        '<td contenteditable="false" class="cell'+c+'">' + arrValue[c].LastName + '</td>' +
+        '<td contenteditable="false" class="cell'+c+'">' + arrValue[c].Email + '</td>' +
+        '<td contenteditable="false" class="cell'+c+'">' + arrValue[c].phoneno + '</td>' +
+        '<td contenteditable="false" class="cell'+c+'">' + arrValue[c].Role + '</td>' +
+        '<td contenteditable="false" class="cell'+c+'">' + arrValue[c].Address + '</td>'+
+        '<td> <button type="button" onclick="editData('+c+')" id="edit'+c+'"> edit data</button></td>'+'<td> <button type="button" onclick="deleteData('+c+')" id="delete'+c+'"> delete data </button></td>';        
+    
+    }
+    document.getElementById("id1").appendChild(table);    
+}
+let stored_tblbody;
+function deleteData(num)
+{    let rowid = "row"+num;
+    if(check===0)
+    { 
+     let deleterow = document.getElementById(rowid);
+     deleterow.parentNode.removeChild(deleterow);
+     deleted_row[num]=true;
+     // let str = row.parentNode.parentNode.rowIndex;
+    // document.getElementById("empTable").deleteRow(str);
+    }
+    else{
+        
+        let presentBody = document.getElementsByTagName("tbody")[0];
+        presentBody.parentNode.replaceChild(stored_tblbody, presentBody);
+        SwitchButton(num);
+        enable_othr_buttons(num);
+    }
+    
+}
+function editData(n)
+{   
+    if(check===0)
+    {   
+        disable_othr_buttons(n);
+        let tblbody_before_edit = document.getElementsByTagName("tbody")[0];
+        stored_tblbody = tblbody_before_edit.cloneNode(true); 
+        let id = "cell"+n;
+        let index;        
+        let ele = document.getElementsByClassName(id);
+            for (index=0; index<ele.length; index++) 
+            {
+            ele[index].setAttribute("contenteditable", "true");
+            }
+            SwitchButton(n);
+    }
+    else
+    {   
+        enable_othr_buttons(n);
+        let id = "cell"+n;
+        let index;
+        let ele = document.getElementsByClassName(id);
+        for (index=0; index<ele.length; index++) {
+            ele[index].setAttribute("contenteditable", "false");
+        }
+        SwitchButton(n);   
+    }
+    
+}
+
+function SwitchButton(b)
+{   id_edit = "edit"+b;
+    id_delete = "delete"+b;
+    if(check===0)//change to save and cancel 
+    {   check =1;
+        document.getElementById(id_edit).innerHTML="save";
+        document.getElementById(id_delete).innerHTML="cancel";
+        
+        
+    }
+    else
+    {
+        check=0;
+        document.getElementById(id_delete).innerHTML="delete data";
+        document.getElementById(id_edit).innerHTML="edit data";
+    }
+}
+function disable_othr_buttons(num)
+{   
+    for(let i =0;i<datalen;i++)
+    {
+        if(deleted_row[i] === true || i===num)
+        {
+            continue;
+        }
+        document.getElementById("edit"+i).disabled=true;
+        document.getElementById("delete"+i).disabled = true;
+    }
+     
+}
+function enable_othr_buttons(num)
+{
+   
+    for(let i=0;i<datalen;i++)
+    {
+        if(deleted_row[i] ===true|| i===num)
+        {
+            continue;
+        }
+        document.getElementById("edit"+i).disabled = false;
+        document.getElementById("delete"+i).disabled = false;
+    }
+}
